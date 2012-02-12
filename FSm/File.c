ca50964db3172c0de7ad9fc05513b22ceeb1c306
diegoserranoa <mail@diegoserranoa.com>
2012-02-12 13:40:43 -0600

    Add header file header.h

--- FSm/File.c
+++ FSm/File.c
@@ -1,6 +1,14 @@
-while(a & b){
-    a=55+3;
-    a++ ;
-    a=55+3;
+
+int a, b;
+int funcion();
+
+int funcion(){
+    while (a < b){
+        a=55+3;
+        a++;
+        a=55+3;
+        
+    }
     
+    return 0;
 }
